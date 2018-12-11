6eddc55dff052fdb46630ec3156bc14e2a3e2036
ton <panupong.pspd@gmail.com>
2018-12-11 10:24:31 +0700

    start 11dec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenComponent } from './components/authen/authen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenComponent
   ],
   imports: [
     BrowserModule,
